af60a3d0afef7e211c77329adac1c107b4475cf3
andreea dan <andreeadan0302@gmail.com>
2024-10-20 21:37:24 +0300

    ex2 s8

--- sprint8/index.js
+++ sprint8/index.js
@@ -9,3 +9,18 @@ console.log(retetaMea.nume)
 console.log(retetaMea.portii)
 console.log(retetaMea.ingrediente)
 
+// ex 2
+const rechizite = {
+    carte: "rosu",
+    caiet: "albastru",
+}
+
+rechizite.coperta = "galben"
+delete rechizite.caiet
+
+const keys = Object.keys(rechizite);
+
+
+console.log(rechizite)
+
+console.log(keys)
