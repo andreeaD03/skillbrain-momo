c18818f99c093be32dd0035fb549da98e6d94265
andreea dan <andreeadan0302@gmail.com>
2024-10-20 21:34:14 +0300

    ex1 s8

---
+++ sprint8/index.js
@@ -0,0 +1,11 @@
+// ex1
+const retetaMea = {
+    nume: "shakshuka",
+    portii: 2,
+    ingrediente : ["ceapa", "ardei", "rosii", "cascaval"],
+}
+
+console.log(retetaMea.nume)
+console.log(retetaMea.portii)
+console.log(retetaMea.ingrediente)
+
