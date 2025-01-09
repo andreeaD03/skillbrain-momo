078b80fde570d5de786a8b9f653e3f0ef5c0259c
andreea dan <andreeadan0302@gmail.com>
2025-01-09 14:34:05 +0200

    tema s13 terminata

--- sprint13-oop/index.js
+++ sprint13-oop/index.js
@@ -1,3 +1,5 @@
+// ex1: Declara o clasa prin care sa reprezinți obiectul numit Mașina.
+
 class Masina {
     constructor(marca, model, culoare, kilometraj) {
         this.marca = marca;
@@ -30,3 +32,44 @@ const masina3 = new Masina("Dacia", "BIGGSTER", "Albastru", "20000");
 console.log(masina1);
 console.log(masina2);
 console.log(masina3);
+
+// ex2:Definește încă o clasa numita MasinaDeCurse. Această clasă va moșteni clasa Masina definita la exercițiul 1, folosind cuvantul cheie extends si va avea un constructor in care se va apela super() (adica superconstructorul).
+
+class MasinaDeCurse extends Masina{
+    constructor(marca, model, culoare, kilometraj) {
+        super()
+        this.marca = marca;
+        this.model = model;
+        this.culoare = culoare;
+        this.kilometraj = kilometraj;
+    }
+
+    getProperties() {
+        return `Marca: ${this.marca}, Model: ${this.model}, Culoare: ${this.culoare}, Kilometraj: ${this.kilometraj}`;
+
+    }
+
+    participaLaCampionat(pozitiaInCampionat) {
+        if (pozitiaInCampionat > 0) {
+            console.log(`am castigat locul ${pozitiaInCampionat}`);
+        } else {
+            console.log("nu am castigat nici un premiu");
+        }
+    }
+  
+}
+
+const campionat = () => {
+    const masinaDeCampionat1 = new MasinaDeCurse("ferrari", "f8", "rosu", "123467");
+    const masinaDeCampionat2 = new MasinaDeCurse("labotghini", "huracan", "galben", "654321");
+
+    console.log("Proprietatile masineiDeCampionat1", masinaDeCampionat1.getProperties());
+    masinaDeCampionat1.participaLaCampionat(1);
+
+    console.log("Proprietatile masineiDeCampionat2", masinaDeCampionat2.getProperties());
+    masinaDeCampionat2.participaLaCampionat(-1)
+}
+
+campionat()
+
+
